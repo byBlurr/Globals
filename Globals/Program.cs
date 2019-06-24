96981590307ec2079d11ded654bd29a2fdc08c02
Blurr <dbldumont11@gmail.com>
2019-06-24 18:56:21 +0100

    Created project

--- Globals/Program.cs
+++ Globals/Program.cs
@@ -4,9 +4,6 @@ namespace Globals
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        static void Main(string[] args){}
     }
 }
