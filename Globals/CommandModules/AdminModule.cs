f5d9bf83d084bca7f47a1e49a7433129f0a06810
Blurr <dbldumont11@gmail.com>
2019-06-28 07:39:03 +0100

    Remove unused code

--- Globals/CommandModules/AdminModule.cs
+++ Globals/CommandModules/AdminModule.cs
@@ -39,21 +39,6 @@ namespace Globals.CommandModules
                 {
                     if (channel.ToLower().Equals(ChannelData.Channels[i].Id)) await ServerConfig.ToggleChannel(Context.Guild.Id, ChannelData.Channels[i].Id, toggle, dbCon);
                 }
-                /*
-                    if (channel.ToLower().Equals("gaming")) await ServerConfig.ToggleChannel(Context.Guild.Id, "gaming", toggle);
-                    else if (channel.ToLower().Equals("music")) await ServerConfig.ToggleChannel(Context.Guild.Id, "music", toggle);
-                    else if (channel.ToLower().Equals("movies")) await ServerConfig.ToggleChannel(Context.Guild.Id, "movies", toggle);
-                    else if (channel.ToLower().Equals("rainbow6")) await ServerConfig.ToggleChannel(Context.Guild.Id, "rainbowsix", toggle);
-                    else if (channel.ToLower().Equals("league")) await ServerConfig.ToggleChannel(Context.Guild.Id, "league", toggle);
-                    else if (channel.ToLower().Equals("rust")) await ServerConfig.ToggleChannel(Context.Guild.Id, "rust", toggle);
-                    else if (channel.ToLower().Equals("gta")) await ServerConfig.ToggleChannel(Context.Guild.Id, "gta", toggle);
-                    else if (channel.ToLower().Equals("pubg")) await ServerConfig.ToggleChannel(Context.Guild.Id, "pubg", toggle);
-                    else if (channel.ToLower().Equals("fortnite")) await ServerConfig.ToggleChannel(Context.Guild.Id, "fortnite", toggle);
-                    else if (channel.ToLower().Equals("apex")) await ServerConfig.ToggleChannel(Context.Guild.Id, "apex", toggle);
-                */
-
-                //string GamingState = "Disabled", MusicState = "Disabled", MoviesState = "Disabled", R6State = "Disabled", LeagueState = "Disabled", RustState = "Disabled", GtaState = "Disabled", PubgState = "Disabled", FortniteState = "Disabled", ApexState = "Disabled";
-                //ServerConfig.GetChannelSettingsAsText(Context.Guild.Id, ref GamingState, ref MusicState, ref MoviesState, ref R6State, ref LeagueState, ref RustState, ref GtaState, ref PubgState, ref FortniteState, ref ApexState);
 
                 var embed = new EmbedBuilder() { Color = new Color(114, 137, 218) };
                 embed.WithDescription("Enable/Disable channels using `!enable <channel> <true/false>`, for example `!enable r6 true`.\nOnce the correct channels are set, use the command `!create`, if this is the initial setup or `!update`, if the bot is already setup on your server.");
@@ -63,19 +48,6 @@ namespace Globals.CommandModules
                 {
                     embed.AddField(new EmbedFieldBuilder() { Name = ChannelData.Channels[i].Id, Value = ServerConfig.GetChannelState(Context.Guild.Id, ChannelData.Channels[i].IndexToggle, dbCon).ToString() });
                 }
-
-                /*
-                embed.AddField(new EmbedFieldBuilder() { Name = "Gaming", Value = GamingState });
-                embed.AddField(new EmbedFieldBuilder() { Name = "Music", Value = MusicState });
-                embed.AddField(new EmbedFieldBuilder() { Name = "Movies", Value = MoviesState });
-                embed.AddField(new EmbedFieldBuilder() { Name = "Rainbow6", Value = R6State });
-                embed.AddField(new EmbedFieldBuilder() { Name = "League", Value = LeagueState });
-                embed.AddField(new EmbedFieldBuilder() { Name = "Rust", Value = RustState });
-                embed.AddField(new EmbedFieldBuilder() { Name = "GTA", Value = GtaState });
-                embed.AddField(new EmbedFieldBuilder() { Name = "PUBG", Value = PubgState });
-                embed.AddField(new EmbedFieldBuilder() { Name = "Fortnite", Value = FortniteState });
-                embed.AddField(new EmbedFieldBuilder() { Name = "Apex", Value = ApexState });
-                */
 
                 embed.WithCurrentTimestamp();
 
--- Globals/Data/ServerConfig.cs
+++ Globals/Data/ServerConfig.cs
@@ -9,9 +9,6 @@ namespace Globals.Data
 {
     class ServerConfig
     {
-        /// <summary>
-        /// Check if the server already has a config.
-        /// </summary>
         public static bool CheckServer(ulong serverid)
         {
             var dbCon = DBConnection.Instance();
@@ -47,66 +44,6 @@ namespace Globals.Data
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-            }
-        }
-
-        public static void GetChannelSettingsAsText(ulong serverid, ref string gamingState, ref string musicState, ref string moviesState, ref string r6State, ref string leagueState, ref string rustState, ref string gtaState, ref string pubgState, ref string fortniteState, ref string apexState)
-        {
-            var dbCon = DBConnection.Instance();
-            dbCon.DatabaseName = BotConfig.Load().DatabaseName;
-
-            if (dbCon.IsConnect())
-            {
-                string query = "SELECT * FROM server_configs WHERE server_id = @serverid";
-                var cmd = new MySqlCommand(query, dbCon.Connection);
-                cmd.Parameters.Add("@serverid", MySqlDbType.UInt64).Value = serverid;
-                var reader = cmd.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    if (reader.GetInt32(12) == 1) gamingState = "Enabled";
-                    if (reader.GetInt32(13) == 1) musicState = "Enabled";
-                    if (reader.GetInt32(14) == 1) moviesState = "Enabled";
-                    if (reader.GetInt32(15) == 1) r6State = "Enabled";
-                    if (reader.GetInt32(16) == 1) leagueState = "Enabled";
-                    if (reader.GetInt32(17) == 1) rustState = "Enabled";
-                    if (reader.GetInt32(18) == 1) gtaState = "Enabled";
-                    if (reader.GetInt32(19) == 1) pubgState = "Enabled";
-                    if (reader.GetInt32(20) == 1) fortniteState = "Enabled";
-                    if (reader.GetInt32(21) == 1) apexState = "Enabled";
-                }
-
-                dbCon.Close();
-            }
-        }
-
-        public static void GetChannelSettingsAsBool(ulong serverid, ref bool gamingState, ref bool musicState, ref bool moviesState, ref bool r6State, ref bool leagueState, ref bool rustState, ref bool gtaState, ref bool pubgState, ref bool fortniteState, ref bool apexState)
-        {
-            var dbCon = DBConnection.Instance();
-            dbCon.DatabaseName = BotConfig.Load().DatabaseName;
-
-            if (dbCon.IsConnect())
-            {
-                string query = "SELECT * FROM server_configs WHERE server_id = @serverid";
-                var cmd = new MySqlCommand(query, dbCon.Connection);
-                cmd.Parameters.Add("@serverid", MySqlDbType.UInt64).Value = serverid;
-                var reader = cmd.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    if (reader.GetInt32(12) == 1) gamingState = true;
-                    if (reader.GetInt32(13) == 1) musicState = true;
-                    if (reader.GetInt32(14) == 1) moviesState = true;
-                    if (reader.GetInt32(15) == 1) r6State = true;
-                    if (reader.GetInt32(16) == 1) leagueState = true;
-                    if (reader.GetInt32(17) == 1) rustState = true;
-                    if (reader.GetInt32(18) == 1) gtaState = true;
-                    if (reader.GetInt32(19) == 1) pubgState = true;
-                    if (reader.GetInt32(20) == 1) fortniteState = true;
-                    if (reader.GetInt32(21) == 1) apexState = true;
-                }
-
-                dbCon.Close();
             }
         }
 
@@ -162,80 +99,6 @@ namespace Globals.Data
                 catch (Exception e)
                 {
                     Console.WriteLine(e.Message);
-                }
-
-                dbCon.Close();
-            }
-        }
-
-        public static void GetChannelIds(ulong serverid, ref ulong gamingId, ref ulong musicId, ref ulong moviesId, ref ulong r6Id, ref ulong leagueId, ref ulong rustId, ref ulong gtaId, ref ulong pubgId, ref ulong fortniteId, ref ulong apexId)
-        {
-            var dbCon = DBConnection.Instance();
-            dbCon.DatabaseName = BotConfig.Load().DatabaseName;
-
-            if (dbCon.IsConnect())
-            {
-                string query = "SELECT * FROM server_configs WHERE server_id = @serverid";
-                var cmd = new MySqlCommand(query, dbCon.Connection);
-                cmd.Parameters.Add("@serverid", MySqlDbType.UInt64).Value = serverid;
-                var reader = cmd.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    if (reader.GetInt64(2) != 0) gamingId = (ulong)reader.GetInt64(2);
-                    if (reader.GetInt64(3) != 0) musicId = (ulong)reader.GetInt64(3);
-                    if (reader.GetInt64(4) != 0) moviesId = (ulong)reader.GetInt64(4);
-                    if (reader.GetInt64(5) != 0) r6Id = (ulong)reader.GetInt64(5);
-                    if (reader.GetInt64(6) != 0) leagueId = (ulong)reader.GetInt64(6);
-                    if (reader.GetInt64(7) != 0) rustId = (ulong)reader.GetInt64(7);
-                    if (reader.GetInt64(8) != 0) gtaId = (ulong)reader.GetInt64(8);
-                    if (reader.GetInt64(9) != 0) pubgId = (ulong)reader.GetInt64(9);
-                    if (reader.GetInt64(10) != 0) fortniteId = (ulong)reader.GetInt64(10);
-                    if (reader.GetInt64(11) != 0) apexId = (ulong)reader.GetInt64(11);
-                }
-
-                dbCon.Close();
-            }
-        }
-
-        public static async Task SetupChannels(ulong serverid, ulong gamingId, ulong musicId, ulong moviesId, ulong r6Id, ulong leagueId, ulong rustId, ulong gtaId, ulong pubgId, ulong fortniteId, ulong apexId)
-        {
-            var dbCon = DBConnection.Instance();
-            dbCon.DatabaseName = BotConfig.Load().DatabaseName;
-
-            if (dbCon.IsConnect())
-            {
-                string query = "SELECT * FROM server_configs WHERE server_id = @serverid";
-                var cmd = new MySqlCommand(query, dbCon.Connection);
-                cmd.Parameters.Add("@serverid", MySqlDbType.UInt64).Value = serverid;
-                var reader = await cmd.ExecuteReaderAsync();
-
-                if (reader.HasRows)
-                {
-                    cmd.Dispose();
-                    query = "UPDATE server_configs SET channel_gaming = @channel_gaming, channel_music = @channel_music, channel_movies = @channel_movies, channel_rainbowsix = @channel_rainbowsix, channel_league = @channel_league, channel_rust = @channel_rust, channel_gta = @channel_gta, channel_pubg = @channel_pubg, channel_fortnite = @channel_fortnite, channel_apex = @channel_apex WHERE server_id = @serverid";
-                    //query = "UPDATE server_configs SET channel_gaming = @channel_gaming, channel_music = @channel_music WHERE server_id = @serverid";
-                    cmd = new MySqlCommand(query, dbCon.Connection);
-                    cmd.Parameters.Add("@serverid", MySqlDbType.UInt64).Value = serverid;
-                    cmd.Parameters.Add("@channel_gaming", MySqlDbType.Text).Value = gamingId;
-                    cmd.Parameters.Add("@channel_music", MySqlDbType.Text).Value = musicId;
-                    cmd.Parameters.Add("@channel_movies", MySqlDbType.Text).Value = moviesId;
-                    cmd.Parameters.Add("@channel_rainbowsix", MySqlDbType.Text).Value = r6Id;
-                    cmd.Parameters.Add("@channel_league", MySqlDbType.Text).Value = leagueId;
-                    cmd.Parameters.Add("@channel_rust", MySqlDbType.Text).Value = rustId;
-                    cmd.Parameters.Add("@channel_gta", MySqlDbType.Text).Value = gtaId;
-                    cmd.Parameters.Add("@channel_pubg", MySqlDbType.Text).Value = pubgId;
-                    cmd.Parameters.Add("@channel_fortnite", MySqlDbType.Text).Value = fortniteId;
-                    cmd.Parameters.Add("@channel_apex", MySqlDbType.Text).Value = apexId;
-
-                    try
-                    {
-                        await cmd.ExecuteNonQueryAsync();
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
                 }
 
                 dbCon.Close();
